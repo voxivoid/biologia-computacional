4c0f9b7d302077e7c39a0d52ae490b0f59204ffc
voxivoid <voxivoid@hotmail.com>
2015-10-01 00:08:46 +0100

    Added traceback and biopython

--- Needleman-Wunsch.py
+++ Needleman-Wunsch.py
@@ -1,37 +1,57 @@
 # coding=utf-8
-
-print("Hello World")
-
+from Bio.SubsMat import MatrixInfo
 
 class Item:
-   value = 0
-   traceback = []
+    value = 0
+    #items on traceback are represented by tuples eg. item matrix[1][1] is represented by the tuple (1,1)
+    traceback = []
 
-   def addItemToTraceback(self, item):
-      self.traceback.append(item)
+    def __init__(self, value):
+        self.value = value
+        self.traceback = []
 
-   def setValue(self, value):
-      self.value = value
+    def __repr__(self):
+        return "V: " + str(self.value) + " Tb: " +str(self.traceback)
+
+    def addItemToTraceback(self, item):
+        self.traceback.append(item)
+
+    def setValue(self, value):
+        self.value = value
+
 
 def initMatrix(s1, s2, gapValue):
-   #size of the strings + gap + string representation
-   matrix = [[0 for i in range(len(s1) + 2)] for j in range(len(s2) + 2)]
+    # size of the strings + gap + string representation
+    matrix = [[0 for i in range(len(s1) + 2)] for j in range(len(s2) + 2)]
 
-   #initializing gap line and column
-   for i in range(0, len(matrix) - 1):
-      matrix[i + 1][1] = gapValue * i
+    # initializing gap line and column
+    for i in range(0, len(matrix) - 1):
+        item = Item(gapValue * i)
+        if(i > 0):
+            item.addItemToTraceback((i,1))
+        matrix[i + 1][1] = item
 
-   for j in range(0, len(matrix[0]) - 1):
-      matrix[1][j + 1] = gapValue * j
+    for j in range(0, len(matrix[0]) - 1):
+        item = Item(gapValue * j)
+        if(j > 0):
+            item.addItemToTraceback((1,j))
+        matrix[1][j + 1] = item
 
-   #inserting strings in the matrix
-   for i in range(0, len(s1)):
-      matrix[i + 2][0] = s1[i]
+    # inserting strings in the matrix
+    for i in range(0, len(s1)):
+        matrix[i + 2][0] = s1[i].upper()
 
-   for j in range(0, len(s2)):
-      matrix[0][j + 2] = s2[j]
+    for j in range(0, len(s2)):
+        matrix[0][j + 2] = s2[j].upper()
 
-   return matrix
+    for i in range(len(matrix)):
+        print matrix[i]
+
+    return matrix
 
 
-initMatrix("teste", "teste", -3)
+def needlemanWunsch(matrix, scoringMatrix, gapValue):
+    return
+
+
+matrix = initMatrix("teste", "teste", -3)
