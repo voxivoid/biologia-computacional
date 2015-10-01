19ab831628caf4ce1bcba851cd5f20bc7126cfd6
voxivoid <voxivoid@hotmail.com>
2015-10-01 18:17:16 +0100

    Done

--- Needleman-Wunsch.py
+++ Needleman-Wunsch.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 from Bio.SubsMat import MatrixInfo
 
+
 class Item:
     value = 0
-    #items on traceback are represented by tuples eg. item matrix[1][1] is represented by the tuple (1,1)
+    # items on traceback are represented by tuples eg. item matrix[1][1] is represented by the tuple (1,1)
     traceback = []
+    visited = False
 
     def __init__(self, value):
         self.value = value
@@ -26,6 +28,9 @@ class Item:
     def getValue(self):
         return self.value
 
+    def getTraceback(self):
+        return self.traceback
+
 
 def initMatrix(s1, s2, gapValue):
     # size of the strings + gap + string representation
@@ -34,14 +39,14 @@ def initMatrix(s1, s2, gapValue):
     # initializing gap line and column
     for line in range(0, len(matrix) - 1):
         item = Item(gapValue * line)
-        if(line > 0):
-            item.addItemToTraceback((line,1))
+        if (line > 0):
+            item.addItemToTraceback((line, 1))
         matrix[line + 1][1] = item
 
     for column in range(0, len(matrix[0]) - 1):
         item = Item(gapValue * column)
-        if(column > 0):
-            item.addItemToTraceback((1,column))
+        if (column > 0):
+            item.addItemToTraceback((1, column))
         matrix[1][column + 1] = item
 
     # inserting strings in the matrix
@@ -51,8 +56,8 @@ def initMatrix(s1, s2, gapValue):
     for line in range(0, len(s2)):
         matrix[line + 2][0] = s2[line].upper()
 
-    for line in range(len(matrix)):
-        print matrix[line]
+    """for line in range(len(matrix)):
+        print matrix[line]"""
 
     return matrix
 
@@ -61,57 +66,76 @@ def needlemanWunsch(matrix, scoringMatrix, gapValue):
     for line in range(2, len(matrix)):
         for column in range(2, len(matrix[0])):
 
-            for l in range(len(matrix)):
+            """for l in range(len(matrix)):
                 print matrix[l]
 
             print "Line: " + str(line) + " Column: " + str(column)
-            print "Score : " + str(scoringMatrix.get((matrix[0][column], matrix[line][0]))) + str(matrix[0][column]) + "," + str(matrix[line][0])
+            print "Score : " + str(scoringMatrix.get((matrix[0][column], matrix[line][0]))) + str(
+                matrix[0][column]) + "," + str(matrix[line][0])"""
             gapUp = matrix[line - 1][column].getValue() + gapValue
             gapLeft = matrix[line][column - 1].getValue() + gapValue
 
-            #this is made because the matrix doesn't have all the combinations eg. there's no (C,I) but there's (I,C)
+            # this is made because the matrix doesn't have all the combinations eg. there's no (C,I) but there's (I,C)
             s = scoringMatrix.get((matrix[0][column], matrix[line][0]))
-            if(s is None):
+            if (s is None):
                 s = scoringMatrix.get((matrix[line][0], matrix[0][column]))
             match = matrix[line - 1][column - 1].getValue() + s
 
-            #traceback is initialized with match item
-            """
-            traceback = [(line - 1, column - 1)]
-            value = match
-
-            if(match < gapUp):
-                traceback = [(line - 1, column)]
-                value = gapUp
-            elif(match == gapUp):
-                traceback.append((line - 1, column))
-
-            if(gapUp < gapLeft):
-                traceback = [(line, column - 1)]
-                value = gapLeft
-            elif(gapUp == gapLeft):
-                traceback.append((line, column - 1))
-            """
             traceback = []
             value = max([gapUp, gapLeft, match])
 
-            if(gapUp == value):
+            if (gapUp == value):
                 traceback = [(line - 1, column)]
 
-            if(gapLeft == value):
+            if (gapLeft == value):
                 traceback = [(line, column - 1)]
 
-            if(match == value):
+            if (match == value):
                 traceback = [(line - 1, column - 1)]
 
             matrix[line][column] = Item(value)
-            matrix[line][column].setTraceback = traceback
+            matrix[line][column].setTraceback(traceback)
 
-            #TODO adicionar o traceback
+            # TODO adicionar o traceback
     return matrix
+
+def traceback(matrix):
+    currentLine = len(matrix) - 1
+    currentColumn = len(matrix[0]) - 1
+    currentItem = matrix[currentLine][currentColumn]
+    s1 = s2 = ""
+    while(currentItem.getTraceback()):
+        tracebackItemCoord = currentItem.getTraceback()[0]
+        if((tracebackItemCoord[0] == (currentLine - 1)) and (tracebackItemCoord[1] == (currentColumn - 1))):
+            #print "Diagonal"
+            s1 = matrix[0][currentColumn] + s1
+            s2 = matrix[currentLine][0] + s2
+            currentLine -= 1
+            currentColumn -= 1
+        elif(tracebackItemCoord[0] == (currentLine - 1)):
+            #print "Cima"
+            s1 = "-" + s1
+            s2 = matrix[currentLine][0] + s2
+            currentLine -= 1
+        elif(tracebackItemCoord[1] == (currentColumn - 1)):
+            #print "Esquerda"
+            s1 = matrix[0][currentColumn] + s1
+            s2 = "-" + s2
+            currentColumn -= 1
+        currentItem = matrix[currentLine][currentColumn]
+
+    x = ""
+    for i in range(len(s1)):
+        x += "|"
+    print "Score: " + str(matrix[len(matrix) - 1][len(matrix[0]) - 1].getValue())
+    print s1
+    print x
+    print s2
+
 
 matrix = initMatrix("WPCIWWPC", "IIWPC", -5)
 matrix = needlemanWunsch(matrix, MatrixInfo.blosum50, -5)
+traceback(matrix)
 
-for line in range(len(matrix)):
-    print matrix[line]
+"""for line in range(len(matrix)):
+    print matrix[line]"""
