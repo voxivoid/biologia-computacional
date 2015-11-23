d42ccfdab37dfd2f5a22312f83ab0a5abf391d52
voxivoid <voxivoid@hotmail.com>
2015-11-23 15:21:40 +0000

    Recombination done

--- Molecular Phylogenetic.py
+++ Molecular Phylogenetic.py
@@ -14,20 +14,42 @@ def simulator(fastaDir, nGen, mr, rr, rfl):
     sequences = []
     for fastaSeq in fastaSequences:
         sequences.append(list(str(fastaSeq.seq)))
-    mutation(sequences, mr)
+    for i in range(nGen):
+        print "::::::::::::: Gen " + str(i) + ":::::::::::::"
+        mutation(sequences, mr)
+        recombination(sequences, rr, rfl)
 
 def mutation(sequences, mr):
+    print "------- Mutation -------"
     for fastaSeq in sequences:
         rngMutate = random.random()
         if(rngMutate <= mr):
             rngPosition = random.randint(0, len(fastaSeq) - 1)
+            print "Pos: " + str(rngPosition)
             while True:
                 rngBase = random.choice("actg")
                 if(rngBase != fastaSeq[rngPosition]):
-                    print rngPosition
                     fastaSeq[rngPosition] = rngBase
                     break
 
         print "".join(fastaSeq)
 
-simulator(u".\\fasta", 0, 1, 0, 0)
+def recombination(sequences, rr, rfl):
+    print "------- Recombination -------"
+    for fastaSeq in sequences:
+        rngRecombination = random.random()
+        if(rngRecombination <= rr):
+            rngPosition = random.randint(0, len(fastaSeq) - 1 - rfl)
+            print "Pos: " + str(rngPosition)
+            while True:
+                rngSequence = random.randint(0, len(sequences) - 1)
+                if(sequences[rngSequence] is not fastaSeq):
+                    print "Seq: " + str(rngSequence)
+                    for i in range(rngPosition, rngPosition + rfl):
+                        fastaSeq[i] = sequences[rngSequence][i].lower()
+                    break
+        print "".join(fastaSeq)
+
+
+
+simulator(u".\\fasta", 3, 1, 1, 4)
