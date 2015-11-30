f71adb2c141b151e807b6c3af59019a47d5f2bec
voxivoid <voxivoid@hotmail.com>
2015-11-30 01:21:04 +0000

    Q2 done. Q1 with errors

---
+++ LAB3 - Q1.py
@@ -0,0 +1,104 @@
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+import random
+import math
+
+def mutation(sequences, mr):
+    #print "------- Mutation -------"
+    for fastaSeq in sequences:
+        rngMutate = random.random()
+        if(rngMutate <= mr):
+            rngPosition = random.randint(0, len(fastaSeq) - 1)
+            #print "Pos: " + str(rngPosition)
+            while True:
+                rngBase = random.choice("ACTG")
+                if(rngBase != fastaSeq[rngPosition]):
+                    fastaSeq[rngPosition] = rngBase
+                    break
+
+        #print "".join(fastaSeq)
+
+def recombination(sequences, rr, rfl):
+    #print "------- Recombination -------"
+    for fastaSeq in sequences:
+        rngRecombination = random.random()
+        if(rngRecombination <= rr):
+            rngPosition = random.randint(0, len(fastaSeq) - 1 - rfl)
+            #print "Pos: " + str(rngPosition)
+            while True:
+                rngSequence = random.randint(0, len(sequences) - 1)
+                if(sequences[rngSequence] is not fastaSeq):
+                    #print "Seq: " + str(rngSequence)
+                    for i in range(rngPosition, rngPosition + rfl):
+                        fastaSeq[i] = sequences[rngSequence][i].upper()
+                    break
+        #print "".join(fastaSeq)
+
+def simulator(fastaDirInput, nGen, mr, rr, rfl):
+    fastaSequences = SeqIO.parse(open(fastaDirInput),'fasta')
+    sequences = []
+    for fastaSeq in fastaSequences:
+        sequences.append(list(str(fastaSeq.seq)))
+    hd = []
+    jc = []
+    for i in range(nGen):
+        #print "::::::::::::: Gen " + str(i) + ":::::::::::::"
+        mutation(sequences, mr)
+        recombination(sequences, rr, rfl)
+        sequencesOutput = []
+        for seq in sequences:
+            sequencesOutput.append("".join(seq))
+
+        x = 0
+        hdTemp = 0
+        jcTemp = 0
+        for i in range(len(sequencesOutput)):
+            for j in range(i+1, len(sequencesOutput)):
+                h = float(hammingDistance(sequencesOutput[i], sequencesOutput[j]))
+                hdTemp += h
+                if (h < 0.75):
+                    jcTemp += jukesCantor(hdTemp)
+                x += 1
+        hdTemp /= float(x)
+        jcTemp /= float(x)
+        hd.append(hdTemp)
+        jc.append(jcTemp)
+    print hd
+    print jc
+
+def hammingDistance(s1, s2):
+    return sum(bool(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2))
+
+def jukesCantor(p):
+    return (-3/4) * math.log1p(1 - (4/3) * p)
+
+def randomSequence(size):
+    dna = ""
+    for count in range(size):
+        dna += random.choice("CGTA")
+    return dna
+
+def initialRandomFasta(fastaDirOutput, size, popsize):
+    records = []
+    rSequence = randomSequence(size)
+    for i in range(popsize):
+        records.append(SeqRecord(Seq(rSequence), "seq" + str(i), "", "", [], [], {}, {}))
+    SeqIO.write(records, fastaDirOutput, "fasta")
+
+def q1():
+    fastaDirInput = u".\\q1.fasta"
+    initialRandomFasta(fastaDirInput, 100, 100)
+
+    simulator(fastaDirInput, 1000, 0.1, 0, 0)
+
+def saveRecordsFasta(sequences, fastaOutputDir):
+    records = []
+    for i in range(1,3):
+        for j in range(len(sequences)/2):
+            records.append(SeqRecord(Seq(sequences[j]), "seq" + str(i-1) + "_" + str(j), "", "", [], [], {}, {}))
+    SeqIO.write(records, fastaOutputDir, "fasta")
+
+
+q1()
+#simulator(u".\\fasta", "", 3, 1, 1, 4)
---
+++ LAB3 - Q2.py
@@ -0,0 +1,95 @@
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+import random
+
+def mutation(sequences, mr):
+    #print "------- Mutation -------"
+    for fastaSeq in sequences:
+        rngMutate = random.random()
+        if(rngMutate <= mr):
+            rngPosition = random.randint(0, len(fastaSeq) - 1)
+            #print "Pos: " + str(rngPosition)
+            while True:
+                rngBase = random.choice("ACTG")
+                if(rngBase != fastaSeq[rngPosition]):
+                    fastaSeq[rngPosition] = rngBase
+                    break
+
+        #print "".join(fastaSeq)
+
+def recombination(sequences, rr, rfl):
+    #print "------- Recombination -------"
+    for fastaSeq in sequences:
+        rngRecombination = random.random()
+        if(rngRecombination <= rr):
+            rngPosition = random.randint(0, len(fastaSeq) - 1 - rfl)
+            #print "Pos: " + str(rngPosition)
+            while True:
+                rngSequence = random.randint(0, len(sequences) - 1)
+                if(sequences[rngSequence] is not fastaSeq):
+                    #print "Seq: " + str(rngSequence)
+                    for i in range(rngPosition, rngPosition + rfl):
+                        fastaSeq[i] = sequences[rngSequence][i].upper()
+                    break
+        #print "".join(fastaSeq)
+
+def simulator(fastaDirInput, nGen, mr, rr, rfl):
+    fastaSequences = SeqIO.parse(open(fastaDirInput),'fasta')
+    sequences = []
+    for fastaSeq in fastaSequences:
+        sequences.append(list(str(fastaSeq.seq)))
+    for i in range(nGen):
+        #print "::::::::::::: Gen " + str(i) + ":::::::::::::"
+        mutation(sequences, mr)
+        recombination(sequences, rr, rfl)
+
+    sequencesOutput = []
+    for seq in sequences:
+        sequencesOutput.append("".join(seq))
+    return sequencesOutput
+
+
+
+def randomSequence(size):
+    dna = ""
+    for count in range(size):
+        dna += random.choice("CGTA")
+    return dna
+
+def initialRandomFasta(fastaDirOutput, size, popsize):
+    records = []
+    for i in range(2):
+        rSequence = randomSequence(size)
+        for j in range(popsize/2):
+            records.append(SeqRecord(Seq(rSequence), "seq" + str(i) + "_" + str(j), "", "", [], [], {}, {}))
+    SeqIO.write(records, fastaDirOutput, "fasta")
+
+def q2():
+    fastaDirInput = u".\\q2.fasta"
+    initialRandomFasta(fastaDirInput, 100,6)
+
+    sequences = simulator(fastaDirInput, 1000, 0.1, 0, 0)
+    saveRecordsFasta(sequences, u".\\q2_1.fasta")
+
+    sequences = simulator(fastaDirInput, 1000, 0.1, 0.1, 5)
+    saveRecordsFasta(sequences, u".\\q2_2.fasta")
+
+    sequences = simulator(fastaDirInput, 1000, 0.1, 0.001, 5)
+    saveRecordsFasta(sequences, u".\\q2_3.fasta")
+
+    sequences = simulator(fastaDirInput, 1000, 0.001, 0.1, 5)
+    saveRecordsFasta(sequences, u".\\q2_4.fasta")
+
+    sequences = simulator(fastaDirInput, 2500, 0.1, 0.01, 5)
+    saveRecordsFasta(sequences, u".\\q2_5.fasta")
+
+def saveRecordsFasta(sequences, fastaOutputDir):
+    records = []
+    for i in range(1,3):
+        for j in range(len(sequences)/2):
+            records.append(SeqRecord(Seq(sequences[j]), "seq" + str(i-1) + "_" + str(j), "", "", [], [], {}, {}))
+    SeqIO.write(records, fastaOutputDir, "fasta")
+
+
+q2()
