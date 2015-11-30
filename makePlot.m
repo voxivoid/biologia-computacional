1dd10bd733ef7d8b17c3f1d8280083d1ab98255a
Patricia <patriciads.pt@gmail.com>
2015-11-30 22:55:06 +0000

    Plot lindinho :)

---
+++ makePlot.m
@@ -0,0 +1,22 @@
+fileID = fopen('q1_output.txt','r');
+
+formatSpec = '%d %f %f';
+sizeA = [3 Inf];
+
+A = fscanf(fileID,formatSpec,sizeA);
+fclose(fileID);
+
+A = A';
+
+x = A(:,1);
+y1 = A(:,2);
+y2 = A(:,3);
+
+figure; hold on
+a1 = plot(x,y1); M1 = '% of mismatching sites/Hamming distance';
+a2 = plot(x,y2); M2 = 'Jukes-Cantor model                     ';
+legend([a1; a2], [M1; M2],'Location','northwest');
+
+title('Sequence Identity Plot')
+xlabel('# generations')
+ylabel('y')
