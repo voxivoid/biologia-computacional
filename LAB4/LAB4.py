7f22c196830d58f1dc286961b61aff2417ec7514
voxivoid <voxivoid@hotmail.com>
2015-12-10 21:44:00 +0000

    Kmeans working (hope so)

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -15,8 +15,7 @@ class Cluster:
         numPoints = len(self.points)
         coords = [p.coords[:-1] for p in self.points]
         unzipped = zip(*coords)
-        r = self.removeUnknowns(unzipped)
-        centroidCoords = [math.fsum(dList)/numPoints for dList in r]
+        centroidCoords = [math.fsum(dList)/numPoints for dList in unzipped]
         return Point(centroidCoords)
 
     def update(self, points):
@@ -26,36 +25,19 @@ class Cluster:
         shift = getDistance(oldCentroid, self.centroid)
         return shift
 
-    def removeUnknowns(self, unzipped):
-        result = []
-        for i in range(len(unzipped)):
-            n = 1
-            sum = 0
-            result.append([])
-            for j in range(len(unzipped[i])):
-                if(unzipped[i][j] == "?"):
-                    result[i].append("")
-                    continue
-                result[i].append(float(unzipped[i][j]))
-                n += 1
-                sum += result[i][j]
-            avg = sum / float(n)
-            for j in range(len(unzipped[i])):
-                if(unzipped[i][j] == "?"):
-                    result[i][j] = avg
-        return result
-
-def kmeans(points, k, cutoff):
+def kmeans(points, k, cutoff, maxItereations):
     centroids = random.sample(points, k) #k random points to be the centroids
     clusters = [Cluster([p]) for p in centroids]
+    iterations = 1
 
     while True:
-        lists = [[]] * k
+        lists = [[] for c in range(k)]
+        print iterations
 
         for p in points:
             smallestDistance = getDistance(p, clusters[0].centroid)
             clusterIndex = 0
-            for i in range(1, k - 1):
+            for i in range(1, k):
                 distance = getDistance(p, clusters[i].centroid)
                 if distance < smallestDistance:
                     smallestDistance = distance
@@ -70,6 +52,11 @@ def kmeans(points, k, cutoff):
 
         if biggestShift < cutoff:
             break
+
+        if iterations > maxItereations:
+            break
+
+        iterations += 1
     return clusters
 
 
@@ -78,18 +65,53 @@ def getDistance(a, b):
     return math.sqrt(ret)
 
 
-def readFromFile():
+def main():
     f = open(u".\\LAB4\\input", "r")
     lines = int(f.readline())
     columns = int(f.readline())
 
     points = []
-
     for i in range(lines):
         line = str(f.readline())[:-1]
         coords = line.split(',')
-        points.append(Point(coords))
+        points.append(coords)
 
-    clusters = kmeans(points, 2, 0.5)
+    f.close()
 
-readFromFile()
+    processedPoints = processPoints(points)
+    processedPoints = [Point(c) for c in processedPoints]
+    clusters = kmeans(processedPoints, 2, 1, 500)
+
+    f = open(u".\\LAB4\\output", "w")
+    for i in range(len(clusters)):
+        f.write("CLUSTER " + str(i) + "\n")
+        f.write("Centroid: " + str(clusters[i].centroid.coords) + "\n")
+        for j in range(len(clusters[i].points)):
+            f.write(str(clusters[i].points[j].coords[-1]) + " " + str(clusters[i].points[j].coords) + "\n")
+    f.close()
+
+def processPoints(points):
+    transposedPoints = [list(a) for a in zip(*points)]
+    transposedProcessedPoints = []
+    for i in range(len(transposedPoints) - 1):
+        transposedProcessedPoints.append([])
+        n = 0
+        sum = 0
+        for j in range(len(transposedPoints[0])):
+            if points[j][i] == "?":
+                transposedProcessedPoints[i].append(points[j][i])
+                continue
+            n += 1
+            sum += float(points[j][i])
+            transposedProcessedPoints[i].append(float(points[j][i]))
+        avg = sum / float(n)
+        for j in range(len(transposedPoints[0])):
+            if points[j][i] == "?":
+                transposedProcessedPoints[i][j] = avg
+    transposedProcessedPoints.append(transposedPoints[-1])
+
+    processedPoints = [list(a) for a in zip(*transposedProcessedPoints)]
+
+    return processedPoints
+
+main()
